d806b3f5ff49bbde38b24ddefc28b57c8a033b24
JoaoVitor <jovioli.dev04@gmail.com>
2023-08-16 01:05:45 -0300

    feat: create project

---
+++ MarkdownManager/MarkdownManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownManager
+//
+//  Created by João Vitor de Oliveira da Silva on 16/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Markdown Manager")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownManager/MarkdownManager/MarkdownManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownManagerApp.swift
+//  MarkdownManager
+//
+//  Created by João Vitor de Oliveira da Silva on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
